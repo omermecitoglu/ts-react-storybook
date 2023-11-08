c253a8e0c412b12947650c7fedf790aa9bc576ef
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2023-11-08 14:53:45 +0700

    add viewports

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -9,6 +9,80 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    viewport: {
+      viewports: {
+        viewport1: {
+          name: "360x800 (xs)",
+          styles: {
+            width: "360px",
+            height: "800px",
+          },
+        },
+        viewport2: {
+          name: "390x844 (xs)",
+          styles: {
+            width: "390px",
+            height: "844px",
+          },
+        },
+        viewport3: {
+          name: "601x962 (sm)",
+          styles: {
+            width: "601px",
+            height: "962px",
+          },
+        },
+        viewport4: {
+          name: "768x1024 (md)",
+          styles: {
+            width: "768px",
+            height: "1024px",
+          },
+        },
+        viewport5: {
+          name: "1024x768 (lg)",
+          styles: {
+            width: "1024px",
+            height: "768px",
+          },
+        },
+        viewport6: {
+          name: "1280x720 (xl)",
+          styles: {
+            width: "1280px",
+            height: "720px",
+          },
+        },
+        viewport7: {
+          name: "1366x768 (xl)",
+          styles: {
+            width: "1366px",
+            height: "768px",
+          },
+        },
+        viewport8: {
+          name: "1440x900 (xxl)",
+          styles: {
+            width: "1440px",
+            height: "900px",
+          },
+        },
+        viewport9: {
+          name: "1536x864 (xxl)",
+          styles: {
+            width: "1536px",
+            height: "864px",
+          },
+        },
+        viewport10: {
+          name: "1920x1080 (xxl)",
+          styles: {
+            width: "1920px",
+            height: "1080px",
+          },
+        },
+      },
+    },
   },
 };
 
